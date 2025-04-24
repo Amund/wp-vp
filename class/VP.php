bd2a7819445c6f6d27a66fdd8243af79f00fd1a9
Amund <mailamund@gmail.com>
2025-04-24 16:35:04 +0200

    remove unused methods

--- class/VP.php
+++ class/VP.php
@@ -5,130 +5,6 @@ class vp
     const PAGE = 'vp';
     const OPTIONS = 'vp-options';
     const CAPABILITY = 'manage_options';
-    const DEBUG_WRAPPER = [
-        'tag' => 'pre',
-        'style' =>
-        'font:12px/13px Consolas,\'Lucida Console\',monospace;text-align:left;color:#ddd;background-color:#222;padding:10px;max-height:500px;overflow:auto;',
-    ];
-
-    /**
-     * Return an HTML tag string, with its attributes and contents
-     * @param string $tag HTML tag name
-     * @param array $attributes HTML tag attributes
-     * @param string $content HTML tag content
-     * @param bool $allow_empty_content Allow empty string as content
-     * @return string HTML tag string
-     */
-    static function html_tag(
-        string $tag,
-        array $attributes = [],
-        string $content = '',
-        bool $allow_empty_content = false,
-    ): string {
-        // empty tag, return empty
-        $tag = trim($tag);
-        if (empty($tag)) {
-            return '';
-        }
-        // https://www.thoughtco.com/html-singleton-tags-3468620
-        $voidElements = [
-            'area' => 1,
-            'base' => 1,
-            'br' => 1,
-            'col' => 1,
-            'command' => 1,
-            'embed' => 1,
-            'hr' => 1,
-            'img' => 1,
-            'input' => 1,
-            'keygen' => 1,
-            'link' => 1,
-            'meta' => 1,
-            'param' => 1,
-            'source' => 1,
-            'track' => 1,
-            'wbr' => 1,
-        ];
-        $has_closing_tag = !isset($voidElements[strtolower($tag)]);
-
-        // prepare attributes output
-        if (count($attributes) > 0) {
-            foreach ($attributes as $key => $value) {
-                if ($value !== '0' && empty($value)) {
-                    $attributes[$key] = ' ' . $key;
-                } else {
-                    $attributes[$key] = ' ' . $key . '="' . htmlspecialchars($value, ENT_QUOTES, 'UTF-8') . '"';
-                }
-            }
-            $attributes = implode('', $attributes);
-        } else {
-            $attributes = '';
-        }
-        if ($has_closing_tag) {
-            if ($content !== '0' && empty($content) && !$allow_empty_content) {
-                $output = '';
-            } else {
-                $output = '<' . $tag . $attributes . '>' . $content . '</' . $tag . '>';
-            }
-        } else {
-            // no closing tag, no content needeed
-            $output = '<' . $tag . $attributes . '>';
-        }
-
-        return $output;
-    }
-
-    /**
-     * Render an array or string to html
-     *
-     * If $data is a string, it is returned as is.
-     * If $data is an array, it can be one of two things:
-     * - an indexed array of strings or arrays, in which case each item is rendered using this function, and the results are concatenated.
-     * - an associative array, in which case the 'tag' key must be present, and optionally the 'content' key.
-     *   The 'tag' key is used to determine the html tag to use, and the 'content' key is used as the content of this tag.
-     *   if the 'content' key is an array, it is rendered using this function, and the result is used as the content of the tag.
-     *   If the 'allow_empty_content' key is present, it is used to determine if an empty content is allowed for the tag.
-     *
-     * @param array|string $data the data to render
-     * @param bool $allow_empty_content whether to allow empty content for the tag
-     * @return string the rendered html
-     */
-    static function render(mixed $data, bool $allow_empty_content = false): string
-    {
-        if (!is_string($data) && !is_array($data)) {
-            $data = (string) $data;
-        }
-
-        if (is_string($data)) {
-            return $data;
-        }
-
-        if (is_array($data)) {
-            if (!array_is_list($data)) {
-                $tag = strtolower($data['tag'] ?? '');
-                unset($data['tag']);
-                $content = $data['content'] ?? '';
-                unset($data['content']);
-                if (isset($data['allow_empty_content'])) {
-                    $allow_empty_content = (bool) $data['allow_empty_content'];
-                    unset($data['allow_empty_content']);
-                }
-
-                if (is_array($content)) {
-                    if (!array_is_list($content)) {
-                        $content = self::render($content);
-                    } else {
-                        $content = implode('', array_map(self::class . '::render', $content));
-                    }
-                }
-                $output = self::html_tag($tag, $data, $content, $allow_empty_content);
-            } else {
-                $output = implode('', array_map(self::class . '::render', $data));
-            }
-        }
-
-        return $output;
-    }
 
     /**
      * Process worpress template part
@@ -153,7 +29,7 @@ class vp
         if (class_exists('VP_Cache') && $useCache) {
             // complete context of part
             $args['part'] = $part;
-            $args['lang'] = defined('LANG') ? LANG : '';
+            $args['lang'] = defined('LANG') ? constant('LANG') : '';
             $post_id = get_the_ID();
             if ($post_id) {
                 $args['post_id'] = $post_id;
@@ -175,13 +51,7 @@ class vp
 
             // error: part not found
             if ($result === false) {
-                $content = vp::render([
-                    'tag' => 'div',
-                    'class' => 'error notice notice-error',
-                    'content' => strtr('<p>part "{part}" not found.</p>', [
-                        '{part}' => $part,
-                    ]),
-                ]);
+                $content = '<div class="error notice notice-error"><p>part "' . $part . '" not found.</p></div>';
             }
 
             // set cache
@@ -202,12 +72,16 @@ class vp
         return $content;
     }
 
-    static function menu($menu_location)
+    static function menu(string $menu_location, array $ctx = []): string
     {
         $cache = VP_CACHE;
         $menu = false;
+        $name = '';
         if ($cache) {
-            $name = $menu_location . '-' . LANG . '-' . get_the_ID();
+            $ctx = ['location' => $menu_location, ...$ctx];
+            ksort($ctx);
+            $name = hash('crc32c', json_encode($ctx));
+            $name = $menu_location . '-' . $name;
             $menu = VP_Cache::get($name, 'menu');
         }
         if ($menu === false) {
@@ -356,62 +230,6 @@ class vp
         ];
 
         return $html;
-    }
-
-    /**
-     * Block rendering function, using a template part with a name equivalent to the block name
-     * ex: for a block 'foo', the template will be themename/part/block/foo.php
-     *
-     * @return void
-     */
-    static function acf_block_render_callback(): void
-    {
-        $func_arg = func_get_args();
-        $part = '';
-        $cache = true;
-        $data = [];
-        if (is_array($func_arg[0]) && !empty($func_arg[0])) {
-            $block = $func_arg[0];
-            $part = !empty($block['name']) ? preg_replace('#^acf/#', 'block/', $block['name']) : $part;
-            $cache = isset($block['vp_cache']) ? (bool) $block['vp_cache'] : $cache;
-            $data = [...$block['data'] ?? [], ...get_fields() ?: []];
-            // $data = get_fields() ?: [];
-            if (($data['example'] ?? false) === 'true') {
-                echo '<img src="' .
-                    get_template_directory_uri() .
-                    '/img/' .
-                    $part .
-                    '.jpg" style="display:block;width:100%; height:auto;"/>';
-                return;
-            }
-        }
-        echo vp::part($part, $data, $cache);
-    }
-
-    // Debug: print_r html formatted
-    static function print($args): void
-    {
-        echo vp::render(array_merge(self::DEBUG_WRAPPER, ['content' => print_r($args, 1)]));
-    }
-
-    // Debug: var_dump html formatted
-    static function dump(...$args): void
-    {
-        ob_start();
-        var_dump(...$args);
-        $dump = ob_get_contents();
-        ob_end_clean();
-        echo vp::render(array_merge(self::DEBUG_WRAPPER, ['content' => $dump]));
-    }
-
-    // Debug: var_export html formatted
-    static function export($args): void
-    {
-        echo vp::render(
-            array_merge(self::DEBUG_WRAPPER, [
-                'content' => var_export($args, 1),
-            ]),
-        );
     }
 
     // Get option value from vp-options, or null
